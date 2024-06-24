ada65be2b904faa0ace150b6284b4569c56e3f8e
Garvielman <153737492+Garvielman@users.noreply.github.com>
2024-06-24 23:11:06 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-st.tittle('Data Mining Prediction Diabetes')
+st.title('Data Mining Prediction Diabetes')
 
 col1, col2 = st.columns(2)
 
@@ -14,48 +14,51 @@ with col2 :
 	Gender = st.text_input ('input Gender')
 
 with col1 :
-	Polyuria = st.text_input ('YES/NO')
+	Polyuria = st.text_input ('input Polyuria')
 
 with col2 :
-	Polydipsia = st.text_input ('YES/NO')
+	Polydipsia = st.text_input ('input Polydipsia')
 
 with col1 :
-	sudden weight loss = st.text_input ('YES/NO')
+	suddenweightloss = st.text_input ('input sudden weight loss')
 
 with col2 :
-	weakness = st.text_input ('YES/NO')
+	weakness = st.text_input ('input weaknes')
 
 with col1 :
-	Polyphagia = st.text_input ('YES/NO')
+	Polyphagia = st.text_input ('input Polyphagia')
 
 with col2 :
-	Genital thrush = st.text_input ('YES/NO')
+	Genitalthrush = st.text_input ('input Genital thrush')
 
 with col1 :
-	visual blurring = st.text_input ('YES/NO')
+	visualblurring = st.text_input ('input visual blurring')
 
 with col2 :
-	Itching = st.text_input ('YES/NO')
+	Itching = st.text_input ('input Itching')
 
 with col1 :
-	Irritability = st.text_input ('YES/NO')
+	Irritability = st.text_input ('input Irritability')
 
 with col2 :
-	delayed healing = st.text_input ('YES/NO')
+	delayedhealing = st.text_input ('input delay healing')
 
 with col1 :
-	partial paresis = st.text_input ('YES/NO')
+	partialparesis = st.text_input ('input partial paresis')
 
 with col2 :
-	muscle stiffness = st.text_input ('YES/NO')
+	musclestiffness = st.text_input ('input muscle stiffness')
 
 with col1 :
-	Alopecia,Obesity = st.text_input ('YES/NO')
+	Alopecia = st.text_input ('input Alopecia')
+	
+with col2 :
+	Obesity = st.text_input ('input Obesity')
 
 diab_diagnosis = ''
 
 if st.button('Test Prediction Diabetes'):
-	diab_prediction = diabetes_model.prediction([[Age,Gender,Polyuria,Polydipsia,sudden weight loss,weakness,Polyphagia,Genital thrush,visual blurring,Itching,Irritability,delayed healing,partial paresis,muscle stiffness,Alopecia,Obesity]])
+	diab_prediction = diabetes_model.predict([[Age, Gender, Polyuria, Polydipsia, suddenweightloss, weakness, Polyphagia, Genitalthrush, visualblurring, Itching, Irritability, delayedhealing, partialparesis, musclestiffness, Alopecia, Obesity]])
 
 	if(diab_prediction[0] == 1):
 		diab_diagnosis = 'Pasien terkena Diabetes'
